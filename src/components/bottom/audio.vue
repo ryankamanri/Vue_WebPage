049a24be1058c32b290fa4a038200c9cffaf4c06
15731214911 <891278127@qq.com>
2020-10-01 21:14:04 +0800

    完成了底部工作

--- src/components/bottom/audio.vue
+++ src/components/bottom/audio.vue
@@ -133,7 +133,7 @@ export default {
   -moz-user-select: none; /*火狐*/
   -webkit-user-select: none; /*webkit浏览器*/
   -ms-user-select: none; /*IE10*/
-  -khtml-user-select: none; /*早期浏览器*/
+  -khtml-user-select: none; /*之前浏览器*/
   user-select: none;
 }
 .audioBtn {
